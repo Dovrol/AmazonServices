425340e6a04aaeaa3bb3fdb2e6dc8de424f39a5b
Dovrol <wiktor.kubis@gmail.com>
2020-12-19 18:46:34 +0100

    Adding wilks method

--- index.py
+++ index.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import random
+
+
 app = Flask(__name__)
+
+lifters = ['Taylor Atwood', 'Chris Duffin', 'Stefi Cohen', 'Russel Orhii']
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/lifter')
+def getLifter():
+    return f'{random.choice(lifters)}, Total: {random.randint(100, 1000)}'
 
-app.run(host="0.0.0.0")
+@app.route('/wilks', methods = ['POST'])
+def wilks():
+    squat = request.json["SQUAT"]
+    bench = request.json["BENCH"]
+    deadlift = request.json["DEADLIFT"]
+
+    return f'{squat + bench + deadlift}'
+
+
+app.run(host="0.0.0.0", debug = True)
